7930eac65b90196f83d919314fa3682bdc8f7c0e
Akshay singh <ww.akshay@gmail.com>
2020-07-24 13:01:01 +0530

    Updates

---
+++ precache-manifest.2c2d913c20380573f5453697083c9f20.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "934b6c0a36b83f31bb360db7a06ab22a",
+    "url": "/imagereco/index.html"
+  },
+  {
+    "revision": "9c041b5100077a71f63a",
+    "url": "/imagereco/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "86f6f964b35e34e0fe46",
+    "url": "/imagereco/static/css/main.129859d9.chunk.css"
+  },
+  {
+    "revision": "9c041b5100077a71f63a",
+    "url": "/imagereco/static/js/2.0f215c51.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/imagereco/static/js/2.0f215c51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86f6f964b35e34e0fe46",
+    "url": "/imagereco/static/js/main.146110fe.chunk.js"
+  },
+  {
+    "revision": "17bd2e86f2f15550d4ba",
+    "url": "/imagereco/static/js/runtime-main.621e4816.js"
+  },
+  {
+    "revision": "a3e4819a75a83fc7103a76e3d5ce4f9e",
+    "url": "/imagereco/static/media/Logo-main.a3e4819a.png"
+  }
+]);
